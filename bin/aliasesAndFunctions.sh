889048fd7fc512a080afe97be26c8d8f50c1df9d
PhilippVWC <apfelphil@googlemail.com>
2021-05-04 14:43:05 +0200

    powerline command prompt for bash
    The go script is named powerline-go
    see https://github.com/justjanne/powerline-go?WT.mc_id=-blog-scottha

--- bin/aliasesAndFunctions.sh
+++ bin/aliasesAndFunctions.sh
@@ -95,4 +95,10 @@ function pth()
 	fi
 }
 
-
+GOPATH=$HOME/go
+function _update_ps1() {
+    PS1="$($GOPATH/bin/powerline-go -error $?)"
+}
+if [ "$TERM" != "linux" ] && [ -f "$GOPATH/bin/powerline-go" ]; then
+    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+fi
