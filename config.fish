37ee7845f2f3c23ed67fb96bfecea6a0a4c33b1a
PhilippVWC <apfelphil@googlemail.com>
2021-06-07 15:59:16 +0200

    feat(config.fish): Add function isSshLogin
    Fish function isSshLogin checks wether current fish session is a remote ssh login session

--- config.fish
+++ config.fish
@@ -18,9 +18,9 @@ abbr --add 'l' 'ls -lstha'
 abbr --add 'vim' 'nvim'
 abbr --add 'ivm' 'nvim'
 abbr --add 'vi' 'nvim'
-abbr --add 'k' "pkill -P $fish_pid"   # kill all daughter processes belonging to the current PID
+abbr --add 'k' "pkill -P $fish_pid" # kill all daughter processes belonging to the current PID
 abbr --add 'untar' 'tar -xcf'
-abbr --add 'ungzip' 'gzip -d' 
+abbr --add 'ungzip' 'gzip -d'
 abbr --add 'matrix' 'cmatrix -s'
 abbr --add 'conf' '$EDITOR $__fish_config_dir/config.fish'
 abbr --add 'settings' '$EDITOR /mnt/c/Users/pvwc/AppData/Local/Packages/Microsoft.WindowsTerminal_8wekyb3d8bbwe/LocalState/settings.json'
@@ -46,11 +46,28 @@ function setKeybindings
     end
 end
 
+function isSshLogin
+    if test -z $SSH_LOGIN
+        set ppid (ps -p (echo $fish_pid) -o ppid=)
+        set ppid (string trim $ppid)
+        set ppcmd (ps -p (echo $ppid) -o comm=)
+        set ppcmd (string trim $ppcmd)
+        if test "$ppcmd" -eq "sshd"
+            set -Ux SSH_LOGIN 0
+        else
+            set -Ux SSH_LOGIN 1
+        end
+        return $SSH_LOGIN
+    else
+        return $SSH_LOGIN
+    end
+end
+
 # Start openSSH authentication agent if 
 # has not happened yet
 function start_ssh_agent
     set e /home/$USER/.ssh/agent.env
-    eval ssh-add -l > /dev/null 2>&1
+    eval ssh-add -l >/dev/null 2>&1
     set s $status
     switch $s
         case 1
@@ -59,13 +76,13 @@ function start_ssh_agent
             ssh-add
         case 2
             echo "Launch openSSH authentication agent:"
-            ssh-agent -c > $e 2>/dev/null
+            ssh-agent -c >$e 2>/dev/null
             source $e
             set -xU SSH_AUTH_SOCK $SSH_AUTH_SOCK
             set -xU SSH_AGENT_PID $SSH_AGENT_PID
             ssh-add
             rm $e
-        end
+    end
 end
 start_ssh_agent
 
@@ -73,5 +90,5 @@ start_ssh_agent
 function fish_prompt
     setKeybindings
     powerline-shell --shell bare $status
-#     eval $GOPATH/bin/powerline-go -error $status -jobs (jobs -p | wc -l)
+    #     eval $GOPATH/bin/powerline-go -error $status -jobs (jobs -p | wc -l)
 end
